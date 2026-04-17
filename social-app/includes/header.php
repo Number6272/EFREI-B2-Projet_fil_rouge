7f4e67ed2ceb63aa9befe46f598ec9969105dae0
Number6272 <julienwu2003@icloud.com>
2026-04-17 12:10:57 +0200

    rename the website

--- social-app/includes/header.php
+++ social-app/includes/header.php
@@ -7,7 +7,7 @@ if (session_status() === PHP_SESSION_NONE) {
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>SocialApp</title>
+    <title>Inter-Knot</title>
     <link rel="stylesheet" href="/test2/social-app/assets/css/style.css">
     <link rel="icon" type="image/png" href="/test2/social-app/assets/images/logo.png">
 </head>
